afb87d9a738c1abb4c3b44e9574b06580daf4685
its-davis <80759908+its-davis@users.noreply.github.com>
2023-02-22 23:02:00 +0530

    host change

--- public/receiver.js
+++ public/receiver.js
@@ -1,4 +1,4 @@
-const webSocket = new WebSocket("wss://localhost:3000")
+const webSocket = new WebSocket("wss://wrtc.djmorris.website:3000")
 
 webSocket.onmessage = (event) => {
     handleSignallingData(JSON.parse(event.data))
